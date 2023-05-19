53b7991aa7929741b29a7697d2ea86d5bfe10955
TIAGO COLADONATO <46878457@ort>
2023-05-19 12:07:00 -0300

    haciendo endpoints api

--- API/API_Tickets/index.js
+++ API/API_Tickets/index.js
@@ -10,15 +10,90 @@ app.use(express.json());
 
 let svc = new TicketService();
 
-app.get('/', async function (req, res) {
+app.get('/API/', async function (req, res) {
     try {
-        let result = await svc.getAll();
+        let result = await svc.getEntradas();
         res.send(result)
     } catch (error) {
         res.send("error")
     }
 })
 
+
+app.get('/API/EventoxEntrada/:id', async function (req, res) {
+    try {
+        let parametros = req.params
+        let result = await svc.getEventoxIdEntrada(parametros.id);
+        res.send(result)
+    } catch (error) {
+        res.send("error")
+    }
+})
+
+app.get('/API/EntradaxIdEntrada/:id', async function (req, res) {
+    try {
+        let parametros = req.params
+        let result = await svc.getEentradaxId(parametros.id);
+        res.send(result)
+    } catch (error) {
+        res.send("error")
+    }
+})
+
+
+app.put('/API/UpdateEntrada/:id', async function (req, res) {
+    try {
+
+        let parametros = req.params
+        let result = await svc.UpdateEntrada(parametros.id)
+
+        res.send(result)
+
+    } catch (error) {
+        res.send("error")
+    }
+
+})
+
+app.post('/API/InsertDatos', async function (req, res) {
+    try {
+
+        // let parametros = req.query
+        // console.log(parametros)
+        // let result = await svc.Insert(parametros.nombre, parametros.glutenFree, parametros.importe, parametros.descripcion)
+
+
+        let parametros = req.params
+
+
+        let result = await svc.InsertarDatos(parametros.adress,parametros.idNFT,parametros.idEntrada)
+
+        res.send(result)
+
+
+
+    } catch (error) {
+        EscribirError(error)
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
     console.log('Example app listening on port ' + port)
-})  
+})
+
+
+// necesita endpoint de EVENTO x idEntrada
+// getEntradaXID
+// uodate entrada (para si tiene NFT O NO)
+//  POST ENTRADA X NFT, usuario, entradaxusuario
+//
--- API/API_Tickets/src/services/TicketService.js
+++ API/API_Tickets/src/services/TicketService.js
@@ -2,21 +2,97 @@ import sql from 'mssql';
 import config from '../../dbconfig.js';
 
 export default class TicketService {
-    getAll = async () => {
-    let resultado = null
+    getEntradas = async () => {
+        let resultado = null
 
-    try {
-        let pool = await sql.connect(config)
-        let result = await pool.request().
-            query("SELECT * FROM ENTRADA")
+        try {
+            let pool = await sql.connect(config)
+            let result = await pool.request().
+                query("SELECT * FROM Evento")
 
-        resultado = result.recordsets[0]
+            resultado = result.recordsets[0]
 
-    } catch (error) {
-        console.log(error)
-      
+        } catch (error) {
+            console.log(error)
+
+        }
+        return resultado;
     }
-    return resultado;
-}
+
+
+    getEventoxIdEntrada = async (id) => {
+        let resultado = null
+
+        try {
+            let pool = await sql.connect(config)
+            let result = await pool.request().input('pId', sql.Int, id).
+                query("exec EventoxIdEntrada @pId")
+
+            resultado = result.recordsets[0]
+
+        } catch (error) {
+            console.log(error)
+
+        }
+        return resultado;
+    }
+
+    getEentradaxId = async (id) => {
+        let resultado = null
+
+        try {
+            let pool = await sql.connect(config)
+            let result = await pool.request().input('pId', sql.Int, id).
+                query("exec getEentradaxId @pId")
+
+            resultado = result.recordsets[0]
+
+        } catch (error) {
+            console.log(error)
+
+        }
+        return resultado;
+    }
+
+
+    UpdateEntrada = async (id) => {
+        let resultado = null
+        try {
+            let pool = await sql.connect(config)
+            let result = await pool.request()
+                .input('pId', sql.Int, id)
+                .query("exec UpdateEntrada @pId")
+            resultado = result.rowsAffected;
+
+        } catch (error) {
+            EscribirError(error)
+        }
+        return resultado
+    }
+
+
+    InsertarDatos = async (adress, idNFT, idEntrada) => {
+        let resultado = null
+        console.log("hola")
+        try {
+            let pool = await sql.connect(config)
+            let result = await pool.request()
+                .input('pAdress', sql.VarChar, adress ?? '')
+                .input('pIdNFT', sql.Int, idNFT ?? '')
+                .input('pIdEntrada', sql.Int, idEntrada ?? '')
+                .query("INSERT INTO Usuario(adress) VALUES (@pAdress)").query("INSERT INTO EntradaxNFT(idEntrada,idNFT) VALUES (@pIdEntrada,@pIdNFT)")
+            resultado = result.rowsAffected;
+
+        } catch (error) {
+            console.log(error)
+        }
+        return resultado
+    }
+
+
+
+
+
+
 
 }
